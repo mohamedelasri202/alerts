6463a2c9c360b5cfca6363e5dfeba40e24fc778e
mohamedelasri202 <el7994995@gmail.com>
2024-12-17 02:41:06 +0100

    creation de la premier alert en html css et js

---
+++ main.js
@@ -0,0 +1,27 @@
+
+var element = document.getElementById("showsweetalert");
+
+
+element.addEventListener('click', function() {
+    console.log('hamid'); 
+    let alrt = document.getElementById("alrtswt");
+    if(alrt.style.display === 'none'){
+        alrt.style.display='flex';
+    }else {
+        alrt.style.display='none'
+    }
+});
+
+var element = document.getElementById("hhh");
+
+
+element.addEventListener('click', function() {
+    console.log('hamid'); 
+    let alrt = document.getElementById("alrtswt");
+    if(alrt.style.display === 'none'){
+        alrt.style.display='flex';
+    }else {
+        alrt.style.display='none'
+    }
+});
+
